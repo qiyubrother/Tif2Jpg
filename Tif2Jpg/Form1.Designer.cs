ea76ac049f3e408ad72799520df494701481311d
zhenhua Liu <qiyubrother@outlook.com>
2019-07-31 14:26:11 +0800

    添加项目文件。

---
+++ Tif2Jpg/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Tif2Jpg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDir = new System.Windows.Forms.TextBox();
+            this.btnDo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tif文件目录";
+            // 
+            // txtDir
+            // 
+            this.txtDir.Location = new System.Drawing.Point(15, 29);
+            this.txtDir.Name = "txtDir";
+            this.txtDir.Size = new System.Drawing.Size(675, 21);
+            this.txtDir.TabIndex = 1;
+            // 
+            // btnDo
+            // 
+            this.btnDo.Location = new System.Drawing.Point(15, 57);
+            this.btnDo.Name = "btnDo";
+            this.btnDo.Size = new System.Drawing.Size(140, 23);
+            this.btnDo.TabIndex = 2;
+            this.btnDo.Text = "开始转换";
+            this.btnDo.UseVisualStyleBackColor = true;
+            this.btnDo.Click += new System.EventHandler(this.btnDo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 93);
+            this.Controls.Add(this.btnDo);
+            this.Controls.Add(this.txtDir);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tif to Jpg";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDir;
+        private System.Windows.Forms.Button btnDo;
+    }
+}
+
---
+++ Tif2Jpg/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+using System.IO;
+
+namespace Tif2Jpg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDo_Click(object sender, EventArgs e)
+        {
+
+            Convert("", "", ConvertCallBack);
+        }
+
+        private void Convert(string inFileName, string outFileName, Action<bool, string, string> callback)
+        {
+            using (Stream imageStreamSource = new FileStream(inFileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                var decoder = new TiffBitmapDecoder(imageStreamSource, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+                var encoder = new JpegBitmapEncoder();
+                encoder.Frames.Add(decoder.Frames[0]);
+
+                using (var stream = new FileStream(outFileName, FileMode.Create))
+                {
+                    encoder.Save(stream);
+                    callback(true, inFileName, outFileName);
+                }
+            }
+        }
+
+        private void ConvertCallBack(bool result, string inFileName, string outFileName)
+        {
+
+        }
+    }
+}
